c8a3e29589120d1e77c2fcbfc45382e377bd3f81
GenuineMas <MasalitinJ@gmail.com>
2024-10-29 20:56:47 +0200

    add healthkit capabilities delete copy of HealthDataFetcher

---
+++ GlucoWatch/GlucoseDataView.swift
@@ -0,0 +1,18 @@
+//
+//  GlucoseDataView.swift
+//  GlucoWatch
+//
+//  Created by Genuine on 29.10.2024.
+//
+
+import SwiftUI
+
+struct GlucoseDataView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GlucoseDataView()
+}
